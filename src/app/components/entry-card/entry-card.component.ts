b61966c8a78df82b30317e5c38b4253dc1408700
Patryk Jablonski <mail@jablo.dev>
2025-05-14 21:03:10 +0200

    Added first version of new table view

--- src/app/components/entry-card/entry-card.component.ts
+++ src/app/components/entry-card/entry-card.component.ts
@@ -4,54 +4,30 @@ import { GameRecord } from '../../models/record.model';
 import { DatePipe, NgClass, NgIf, NgStyle } from '@angular/common';
 import { RecordType } from '../../enum/type.enum';
 import { Router } from '@angular/router';
-import { StatService } from '../../service/stat.service';
+import { DataDisplayService } from '../../service/data-display.service';
 
 @Component({
     selector: 'app-entry-card',
+    standalone: true,
     imports: [CardModule, NgIf, DatePipe, NgStyle, NgClass],
-    providers: [StatService],
     templateUrl: './entry-card.component.html',
     styleUrl: './entry-card.component.scss'
 })
 export class EntryCardComponent {
     @Input() gameRecord: GameRecord | undefined;
-    router: Router = inject(Router)
-    statService: StatService = inject(StatService);
-    score: number = 0;
+    router = inject(Router);
+    display = inject(DataDisplayService);
 
-    constructor() {}
+    score: number = 0;
 
     ngOnChanges(changes: SimpleChanges): void {
         if (changes['gameRecord'] && this.gameRecord) {
-            this.score = this.statService.getTotalScore(this.gameRecord);
+            this.score = this.display.getTotalScore(this.gameRecord);
         }
     }
 
     getRecordTypeLabel(type: keyof typeof RecordType | undefined): string {
         return type && type !== 'FULL' ? RecordType[type] : '';
-    }
-
-    isTierScore(score: number): boolean {
-        return score >= 85;
-    }
-
-    getTierGradient(score: number): string {
-        if (score >= 95) {
-            return 'radial-gradient(circle, #FFD700 40%, #ffb6c1 100%)';
-        } else if (score >= 90) {
-            return 'radial-gradient(circle, #C0C0C0 40%, #90ee90 100%)';
-        } else if (score >= 85) {
-            return 'radial-gradient(circle, #e49a6e 30%, #b4693e 90%)';
-        }
-        return 'transparent';
-    }
-
-    getScoreTextColor(score: number): string {
-        if (score >= 80) return 'limegreen';
-        if (score >= 75) return 'yellowgreen';
-        if (score >= 70) return 'yellow';
-        if (score >= 65) return 'orange';
-        return 'red';
     }
 
     goToDetail(): void {
