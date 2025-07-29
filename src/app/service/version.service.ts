ece67446ef327408975a27b9f94092d0e49ed56f
Patryk Jablonski <mail@jablo.dev>
2025-07-29 17:03:23 +0200

    Updated version

--- src/app/service/version.service.ts
+++ src/app/service/version.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class VersionService {
-    private readonly version = '4.2.1';
+    private readonly version = '4.2.2';
 
     getVersion(): string {
         return this.version;
